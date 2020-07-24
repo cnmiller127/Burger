8f621c7255421939ede4c332df7b20f2c733cf1c
Christian Miller <cnmiller127@gmail.com>
2020-07-24 18:35:20 -0500

    mysql config to jaws db

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burgers_db", "root", "superBase93!", {
-  host: "localhost",
+var sequelize = new Sequelize("wwlvq364biwe7iti", "l9ix4yyu0pm9alq9", "de79hvik2ry49xlu", {
+  host: "blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
   pool: {
