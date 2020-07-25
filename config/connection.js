e23a298c01893dc04ce9359b9b0515dc9a6ebbd8
Christian Miller <cnmiller127@gmail.com>
2020-07-24 19:51:26 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("kt4r2uupk9yoiqln", "ie7yvva9p8e2tnbt", "fenzgruvpbgq40bk", {
-  host: "dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+var sequelize = new Sequelize("ylomb0mvkiz5iz2s", "tw8g6mwdyi98u9vt", "xesapi94813zgnf3", {
+  host: "i2cpbxbi4neiupid.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
   pool: {
